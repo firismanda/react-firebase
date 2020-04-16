f230c27c016fd9fdd331ec14c45f4c198976b3d4
firismanda <43681154+firismanda@users.noreply.github.com>
2020-04-16 14:06:23 +0700

    Add files via upload
    done

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDGe7ElJo_8xK6L5xbppS7hs2Wg0Mq4GuA",
+    authDomain: "react-firebase-3083c.firebaseapp.com",
+    databaseURL: "https://react-firebase-3083c.firebaseio.com",
+    projectId: "react-firebase-3083c",
+    storageBucket: "react-firebase-3083c.appspot.com",
+    messagingSenderId: "423564510843",
+    appId: "1:423564510843:web:4f8026eecadde6cdb2c4de",
+    measurementId: "G-BCG0LR2M1W"
+};
